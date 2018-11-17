2f4b106d4629f859768f1c9e290280759fe8f3fb
root <root@ip-172-31-24-100.us-east-2.compute.internal>
2018-11-17 10:37:46 +0000

    latest update

---
+++ javajar/src/main/java/reddy/App.java
@@ -0,0 +1,14 @@
+package reddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "This is a newly created Maven file" );
+    }
+}
